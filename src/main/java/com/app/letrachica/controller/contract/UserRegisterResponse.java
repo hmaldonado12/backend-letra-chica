548f5d45bb2f031be6de7a4cd14136e1d7bd2650
Daniel Mamani <danymamani1002@gmail.com>
2025-05-19 13:38:56 -0300

    feat: Implement register response dto

--- src/main/java/com/app/letrachica/controller/contract/UserRegisterResponse.java
+++ src/main/java/com/app/letrachica/controller/contract/UserRegisterResponse.java
@@ -1,4 +1,16 @@
 package com.app.letrachica.controller.contract;
 
 public class UserRegisterResponse {
+    private String message;
+
+    public UserRegisterResponse() {}
+
+    public UserRegisterResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() { return message; }
+    public void setMessage(String message) { this.message = message; }
+
+
 }
