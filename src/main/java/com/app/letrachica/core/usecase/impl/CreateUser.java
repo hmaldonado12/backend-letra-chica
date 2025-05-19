65ee80896e1d84434ffe825817e6b10afab1473a
Daniel Mamani <danymamani1002@gmail.com>
2025-05-19 12:11:51 -0300

    feat: add use case CreateUser

--- src/main/java/com/app/letrachica/core/usecase/impl/CreateUser.java
+++ src/main/java/com/app/letrachica/core/usecase/impl/CreateUser.java
@@ -1,4 +1,21 @@
 package com.app.letrachica.core.usecase.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.app.letrachica.core.gateway.RegisterUserRepository;
+
+@Service
 public class CreateUser {
+
+        private final RegisterUserRepository registerUserRepository;
+
+        @Autowired
+        public CreateUser(RegisterUserRepository registerUserRepository) {
+                this.registerUserRepository = registerUserRepository;
+        }
+
+        public void execute(String userId, String name, String email, String password, String phoneNumber, String address) {
+                registerUserRepository.registerUser(userId, name, email, password, phoneNumber, address);
+        }
 }
