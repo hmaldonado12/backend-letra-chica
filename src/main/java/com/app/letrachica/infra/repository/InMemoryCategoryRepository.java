974cd64c93ba301c57379b227cb13383ae889447
Daniel Mamani <danymamani1002@gmail.com>
2025-06-03 21:19:01 -0300

    feat(category): add in-memory repository for categories
    - Added CategoryRepository interface to core/gateway.
    - Implemented InMemoryCategoryRepository for development and testing.

---
+++ src/main/java/com/app/letrachica/infra/repository/InMemoryCategoryRepository.java
@@ -0,0 +1,35 @@
+package com.app.letrachica.infra.repository;
+
+import java.util.*;
+
+import org.springframework.stereotype.Repository;
+
+import com.app.letrachica.core.domain.Category;
+import com.app.letrachica.core.gateway.CategoryRepository;
+
+@Repository
+public class InMemoryCategoryRepository implements CategoryRepository {
+    private final Map<String, Category> categories = new HashMap<>();
+
+    @Override
+    public Optional<Category> findById(String id) {
+        return Optional.ofNullable(categories.get(id));
+    }
+
+    @Override
+    public Optional<Category> findByName(String name) {
+        return categories.values().stream()
+                .filter(category -> category.getName().equals(name))
+                .findFirst();
+    }
+
+    @Override
+    public List<Category> findByAll() {
+        return new ArrayList<>(categories.values());
+    }
+
+    @Override
+    public void save(Category category) {
+        categories.put(category.getId(), category);
+    }
+}
