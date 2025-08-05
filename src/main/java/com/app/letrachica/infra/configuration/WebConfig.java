bc40dab3b588a28de8ef540acd982bde942dc6a9
hmaldonado12 <hmaldonado@fi.uba.ar>
2025-08-05 16:41:31 -0300

    nuevo commit, all origins

--- src/main/java/com/app/letrachica/infra/configuration/WebConfig.java
+++ src/main/java/com/app/letrachica/infra/configuration/WebConfig.java
@@ -16,7 +16,7 @@ public class WebConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 String[] origins = allowedOrigins.split(",");
                 registry.addMapping("/**")
-                        .allowedOrigins(origins)
+                        .allowedOrigins("*")
                         .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(true);
